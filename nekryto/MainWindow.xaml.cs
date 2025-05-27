230b795e67d63210779a44a56bc4e790843a6842
Kostenkov_GM <Kostenkov_GM@ISTIS-COL-10309>
2025-05-27 07:00:54 +0300

    Добавьте файлы проекта.

---
+++ nekryto/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace nekryto
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DBEntities db=new DBEntities();
+        public MainWindow()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+
+            var enter = db.Users.FirstOrDefault(x => x.UserName == UsernameTextBox.Text && x.Password == PasswordBox.Password);
+
+            if (enter == null)
+            {
+                MessageBox.Show("Такого пользователя нет!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                MessageBox.Show($"Добро пожаловать, {UsernameTextBox.Text}!", "Успешный вход", MessageBoxButton.OK, MessageBoxImage.Information);
+                var productsWindow = new Products(UsernameTextBox.Text);
+                productsWindow.Show();
+                this.Close();
+            }
+        }
+
+        private void Guest_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
