ce24100c489f5975f2519d617c0b9b0b109ad4e6
Malcev_AA <Malcev_AA@ISTIS-COL-10310>
2025-05-27 10:20:33 +0600

    круто+- сделано много че

--- nekryto/Products.cs
+++ nekryto/Products.cs
@@ -21,6 +21,8 @@ namespace nekryto
         }
     
         public int ProductID { get; set; }
+        public decimal Discount { get; set; }
+        public decimal DiscountedPrice { get; set; }
         public string ProductName { get; set; }
         public string Brand { get; set; }
         public decimal Price { get; set; }
--- nekryto/Products.xaml.cs
+++ nekryto/Products.xaml.cs
@@ -19,9 +19,41 @@ namespace nekryto
     /// </summary>
     public partial class Products : Window
     {
+        DBEntities db = new DBEntities();
+        private List<Products> products;
         public Products(string username)
         {
             InitializeComponent();
+            ProductsListView.ItemsSource = db.Products.ToList();
+
+            products = db.Products.ToList();
+
+            //var orders = db.Orders.ToList();
+           //decimal maxSales = orders.Sum(o => o.TotalCost);
+
+            //foreach (var product in products)
+            //{
+            //   // product.Discount = CalculateDiscount(maxSales, totalSales);
+            //    product.DiscountedPrice = product.Price * (1 - product.Discount / 100);
+            //}
+
+        }
+        private decimal CalculateDiscount(decimal maxSales, decimal totalSales)
+        {
+            if (totalSales < maxSales / 4) return 0;
+            if (totalSales < maxSales / 2) return 5;
+            if (totalSales < (3 * maxSales) / 4) return 10;
+            return 15;
+        }
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+            {
+                Products products = (Products)e.AddedItems[0];
+
+                string ProductName = products.ProductName;
+
+            }
         }
     }
 }
